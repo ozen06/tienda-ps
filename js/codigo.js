c6c417e8399331c96d43772b5f61d0a8de7693ae
oZen <enzo2bengolea@gmail.com>
2026-06-16 00:15:15 -0300

    Añadi: 1-Tienda, 2-Implemente Filtros

--- js/codigo.js
+++ js/codigo.js
@@ -5,7 +5,7 @@
 const products = [
   // --- STEAM ---
   { Nombre: "GTA 5", Precio: 5, Genero: "Accion", Plataforma: "Steam" },
-  { Nombre: "Cyberpunk 2077", Precio: 30, Genero: "RPG / Accion", Plataforma: "Steam" },
+  { Nombre: "Cyberpunk 2077", Precio: 30, Genero: ["RPG", "Accion"], Plataforma: "Steam" },
 
   // --- PSN ---
   { Nombre: "The Last of Us Part I", Precio: 70, Genero: "Aventura", Plataforma: "PSN" },
@@ -13,7 +13,7 @@ const products = [
 
   // --- EPIC GAMES ---
   { Nombre: "Alan Wake 2", Precio: 50, Genero: "Terror", Plataforma: "Epic" },
-  { Nombre: "Red Dead Redemption 2", Precio: 40, Genero: "Accion / Aventura", Plataforma: "Epic" },
+  { Nombre: "Red Dead Redemption 2", Precio: 40, Genero: ["Accion", "Aventura"], Plataforma: "Epic" },
 
   // --- XBOX ---
   { Nombre: "Halo Infinite", Precio: 40, Genero: "Shooter", Plataforma: "Xbox" },
@@ -22,6 +22,8 @@ const products = [
 
 // Estado dinámico de la app (variables que cambian con la interacción del usuario)
 const datos = [[], [], [], []];
+const filtrosActivos = [];
+let itemFiltrados = [];
 let contadorCarrito = 0;
 
 /* ==========================================================================
@@ -29,6 +31,7 @@ let contadorCarrito = 0;
    ========================================================================== */
 // Agrupá todos los document.getElementById o querySelector acá arriba
 const inventario = document.getElementById('EymrNHzbVl')
+const filtros = document.getElementById('filtros');
 const listaGeneros = document.getElementById('f-generos');
 const listaPrecio = document.getElementById('f-precio');
 const listaPlataforma = document.getElementById('f-plataforma');
@@ -38,18 +41,18 @@ const carrito = document.getElementById('Carrito');
    3. LOGICA PRINCIPAL / RENDERIZADO (Funciones que dibujan en pantalla)
    ========================================================================== */
 // Función encargada exclusivamente de pintar las tarjetas en el HTML
-function tarjetas(datos) {
-  products.forEach((elemento, indice) => {
+function tarjetas(arreglo) {
+  inventario.innerHTML = "";
+  arreglo.forEach((elemento, indice) => {
     const nombre = elemento.Nombre;
     const precio = elemento.Precio;
-    const genero = elemento.Genero;
     const plataforma = elemento.Plataforma;
-    impresion(nombre, precio, plataforma, indice);
+    impresion(nombre, precio, plataforma);
   })
 }
 
-function impresion(nombre, precio, plataforma, row) {
-if (plataforma === 'Steam') {
+function impresion(nombre, precio, plataforma) {
+  if (plataforma === 'Steam') {
     inventario.innerHTML += `
         <div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4">
                 <div class="cards-${plataforma.toLowerCase()} overflow-hidden rounded-4 p-3 d-flex flex-column gap-2 h-100 shadow">
@@ -120,7 +123,7 @@ if (plataforma === 'Steam') {
   }
 }
 
-function filtros(datos) {
+function filtrado(datos) {
   datos[1].forEach((elemento) => {
     listaGeneros.innerHTML += `
         <li><label class="dropdown-item"><input type="checkbox" value="${elemento}"> ${elemento}</label></li>
@@ -128,12 +131,12 @@ function filtros(datos) {
   })
   datos[2].forEach((elemento) => {
     listaPrecio.innerHTML += `
-        <li><label class="dropdown-item"><input type="checkbox" value="${elemento}"> ${elemento}</label></li>
+        <li><label class="dropdown-item"><input type="radio" name="precio-unico" value="${elemento}"> ${elemento}</label></li>
     `;
   })
   datos[3].forEach((elemento) => {
     listaPlataforma.innerHTML += `
-        <li><label class="dropdown-item"><input type="checkbox" value="${elemento}"> ${elemento}</label></li>
+        <li><label class="dropdown-item"><input type="radio" name="plataforma-unica" value="${elemento}"> ${elemento}</label></li>
     `;
   })
 }
@@ -148,10 +151,22 @@ ordenamiento(datos[1], "Genero");
 ordenamiento(datos[2], "Precio");
 ordenamiento(datos[3], "Plataforma");
 
+console.log(products[1].Genero)
+
 function ordenamiento(arreglo, propiedad) {
   products.map((elemento) => {
     const actual = elemento[propiedad];
+    if (Array.isArray(elemento[propiedad])) {
+      actual.forEach(genero => {
+        const borrar = arreglo.findIndex((elemento) => elemento == genero);
+        if (borrar === -1) {
+          arreglo.push(genero);
+        }
+      })
+    }
     const borrar = arreglo.findIndex((elemento) => elemento == actual);
+
+    // accion
     if (borrar === -1) {
       if (typeof actual == "string") {
         arreglo.push(actual);
@@ -165,6 +180,31 @@ function ordenamiento(arreglo, propiedad) {
   });
 }
 
+function comprobacion(propiedad) {
+  let tipoPropiedad = "";
+  datos[0].forEach(elemento => {
+    if (elemento === propiedad) {
+      tipoPropiedad = "Nombre"
+    }
+  })
+  datos[1].forEach(elemento => {
+    if (elemento === propiedad) {
+      tipoPropiedad = "Genero"
+    }
+  })
+  datos[2].forEach(elemento => {
+    if (elemento === Number(propiedad)) {
+      tipoPropiedad = "Precio"
+    }
+  })
+  datos[3].forEach(elemento => {
+    if (elemento === propiedad) {
+      tipoPropiedad = "Plataforma"
+    }
+  })
+  return tipoPropiedad
+}
+
 /* fetch('https://www.steamgriddb.com/api/v2')
   .then(response => response.json())
   .then(data => console.log(data)); */
@@ -174,28 +214,66 @@ function ordenamiento(arreglo, propiedad) {
    ========================================================================== */
 // Captura de clics, inputs, envíos de formularios. Van al final porque llaman a las funciones de arriba.
 
-
 inventario.addEventListener('click', (event) => {
   if (event.target.closest('.btn-compra')) {
     contadorCarrito += 1;
     carrito.innerText = contadorCarrito;
   }
 });
+filtros.addEventListener('click', (event) => {
+  const elemento = event.target;
+  if (elemento.tagName == "INPUT") {
+    const actual = elemento.value;
+    dato = comprobacion(actual);
 
-/* botonCarrito.addEventListener('click', () => {
-    console.log("Abriendo carrito...");
+    if(dato === "Plataforma"){
+      filtrosActivos.forEach((elemento, indice) =>{
+        if (comprobacion(elemento) === "Plataforma"){
+          filtrosActivos.splice(indice, 1)
+        }
+      })
+    } else if(dato === "Precio"){
+      filtrosActivos.forEach((elemento, indice) =>{
+        if (comprobacion(elemento) === "Precio"){
+          filtrosActivos.splice(indice, 1)
+        }
+      })
+    }
+
+    const borrar = filtrosActivos.findIndex((elemento) => elemento == actual);
+    if (borrar == -1) {
+      filtrosActivos.push(actual);
+    } else {
+      filtrosActivos.splice(borrar, 1);
+    }
+
+    if (filtrosActivos.length === 0) {
+      tarjetas(products);
+      return;
+    }
+
+    itemFiltrados = products.filter(juego => {
+      return filtrosActivos.every((filtro) => {
+        dato = comprobacion(filtro)
+        if (dato == "Genero") {
+          return juego.Genero.includes(filtro)
+        } else if (dato == "Precio") {
+          return juego.Precio == Number(filtro)
+        } else if (dato == "Plataforma") {
+          return juego[dato] === filtro
+        }
+        return juego[dato] === filtro
+      })
+    });
+
+    tarjetas(itemFiltrados)
+  }
 });
-// Ejemplo para capturar los filtros (usando delegación de eventos o listeners individuales)
-dropdownGeneros.addEventListener('change', (e) => {
-    const generoSeleccionado = e.target.value;
-    // Ejecuta lógica de filtrado y vuelve a renderizar
-}); */
-
 /* ==========================================================================
    6. INICIALIZACIÓN DE LA APLICACIÓN (Punto de arranque)
    ========================================================================== */
 // Código que se ejecuta apenas se termina de cargar la página para que la tienda no empiece vacía
 document.addEventListener('DOMContentLoaded', () => {
-  tarjetas(datos);
-  filtros(datos);
+  tarjetas(products);
+  filtrado(datos);
 });
